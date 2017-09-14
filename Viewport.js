015fde241561670eb69db32f23bcb74a01dd83e1
AJTrowell <ajtrowell@gmail.com>
2017-09-13 20:59:38 -0500

    Adding support for optional Viewport constructor arguments.
    Error checking is still needed in several places, and the
    setSize() and setZoom() functions need to be written to
    keep Viewport.sizeVector and Viewport.zoom consistent with
    one another.

--- Viewport.js
+++ Viewport.js
@@ -1,14 +1,32 @@
 // Viewport class constructor
 // Map sim space (in meters) to canvas space (in pixels)
 // through a viewport.
-function Viewport() {
+// Note: setZoom() and setSizeVector() should BOTH modify this.zoom and
+// this.sizeVector to keep them consistent.
+function Viewport(positionVector, sizeVector) {
     // Position and Size are in sim space, not canvas pixels.
-    this.positionVector = createVector(0,0); // position of viewport center.
+    // Optional Arguments (with error checking)
+    if(positionVector) { // Optional argument
+        // DEBUG: Add error checking for p5.vector type
+        this.positionVector = positionVector.copy();
+    } else {
+        this.positionVector = createVector(0,0); // position of viewport center.
+    }
+
+    if(sizeVector) { // Optional argument
+        // DEBUG: Add error checking for p5.vector type
+        this.sizeVector = sizeVector.copy(); // Initialization.
+        this.setSizeVector(sizeVector); // Employ aspect ratio correction.
+    } else {
+        this.sizeVector = createVector(10,10); // x,y
+        this.setSizeVector(this.sizeVector.copy()); // Employ aspect ratio correction.
+        // DEBUG: Is copy required? ^
+    }
+
     this.zoom = //pixelsPerMeter
-    this.sizeVector = createVector(10,10); // x,y
 }
 
-Viewport.prototype.setPosition = function(postionVector) {
+Viewport.prototype.setPosition = function(positionVector) {
     this.positionVector = positionVector;
 }
 Viewport.prototype.setZoom = function(pixelsPerMeter) {
@@ -17,10 +35,13 @@ Viewport.prototype.setZoom = function(pixelsPerMeter) {
 Viewport.prototype.setPixelsPerMeter = function(pixelsPerMeter) {
     this.setZoom(pixelsPerMeter);
 }
-Viewport.prototype.setSize = function(sizeVector) {
+Viewport.prototype.setSizeVector = function(sizeVector) {
     // Ensure render remains square. Apply consistent zoom (pixelsPerMeter) 
     // to both x and y axes.
-    this.sizeVector = sizeVector; // DEBUG: Needs squarenss checking.
+    this.sizeVector = sizeVector.copy(); // DEBUG: Needs squarenss checking.
+}
+Viewport.prototype.setSize= function(widthMeters, heightMeters) {
+    this.sizeVector = createVector(widthMeters, heightMeters); 
 }
 Viewport.prototype.isObjectInView = function(entity) {
     // Given entity is expected to have a positionVector property.
